1fefab294c7a95d787a72a8d765ae27a894cfa42
gracy <chrisdexterity001@gmail.com>
2019-07-18 15:48:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/realmdatabase/adapters/RealmBooksAdapter.java
@@ -0,0 +1,15 @@
+package com.example.realmdatabase.adapters;
+
+import android.content.Context;
+
+import com.example.realmdatabase.model.Book;
+
+import io.realm.RealmResults;
+
+public class RealmBooksAdapter extends RealmModelAdapter<Book> {
+
+    public RealmBooksAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+
+        super(context, realmResults, automaticUpdate);
+    }
+}
